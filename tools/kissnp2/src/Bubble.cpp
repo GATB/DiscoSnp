cc1a9733e26004a0a463d6f549242df837a984d6
Pierre Peterlongo <pierre.peterlongo@inria.fr>
2015-01-22 17:45:41 +0100

    bubble

--- tools/kissnp2/src/Bubble.cpp
+++ tools/kissnp2/src/Bubble.cpp
@@ -59,8 +59,9 @@ BubbleFinder::BubbleFinder (IProperties* props, const Graph& graph, Stats& stats
     max_breadth = props->getInt (STR_BFS_MAX_BREADTH);
     /** We set the traversal kind. */
     traversalKind = Traversal::NONE;
-    if (props->get(STR_DISCOSNP_TRAVERSAL_UNITIG) != 0)  { traversalKind = Traversal::UNITIG; }
-    if (props->get(STR_DISCOSNP_TRAVERSAL_CONTIG) != 0)  { traversalKind = Traversal::CONTIG; }
+    printf("HEY\n");
+    if (props->get(STR_DISCOSNP_TRAVERSAL_UNITIG) != 0)  { traversalKind = Traversal::UNITIG; printf("HEY unitig !!!\n");}
+    if (props->get(STR_DISCOSNP_TRAVERSAL_CONTIG) != 0)  { traversalKind = Traversal::CONTIG; printf("HEY contig !!!\n");}
     
     /** We set the name of the output file. */
     stringstream ss;
