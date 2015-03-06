a8b836c8cafe951a55d97b2bb318bba808faac48
chloe riou <chloe.riou@inria.fr>
2015-03-06 13:16:45 +0100

    Creation of VCF_creator

---
+++ run_pipeline_VCF_creator.sh
@@ -0,0 +1,236 @@
+#!/bin/bash
+
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+remove=1
+PATH_VCF_creator=""
+samfile=""
+vcffile=""
+genome=""
+PATH_BWA=""
+discoSNPs=""
+l=""
+n=""
+s=""
+
+
+function help {
+echo " ##############################"
+echo "   Run VCF_creator pipeline     "
+echo " ##############################"
+echo "Usage : ./run_pipeline_VCF_creator.sh OPT"
+echo -e "\t-h : print this message"
+echo -e "\t-b : path where bwa is"
+echo -e "\t-c : path where VCF_creator is"
+echo -e "\t-g : reference genome (.fasta) for alignment with snps from discosnp++ with path"
+echo -e "\t-p : snps from discosnp++ with path"
+echo -e "\t-s : bwa option : distance with the seed"
+echo -e "\t-n : bwa option : allowed distance with the reference for a snp"
+echo -e "\t-l : bwa option : lenght of the seed for alignment"
+echo -e "\t-f : <file>.sam : skip the algnment phases to create the vcf file "
+echo -e "\t-o : output <file>.vcf"
+echo -e "\t-w : remove tmp files (index files)"
+
+}
+
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------
+
+while getopts "hb:c:g:p:l:n:s:wf:o:" opt; do
+case $opt in
+
+	w)
+	remove=0
+	;;
+
+	h)
+	help
+	exit 1
+	;;
+
+	b)
+	echo -e "\t##BWA directory: $OPTARG" >&2
+	PATH_BWA=$OPTARG
+	;;
+
+	c)
+	echo -e "\t##VCF_creator directory: $OPTARG" >&2
+	PATH_VCF_creator=$OPTARG
+	;;
+
+	g)
+	echo -e "\t##use genome : $OPTARG" >&2
+	genome=$OPTARG
+	;;
+
+	p)
+	echo -e "\t##use disco SNPS : $OPTARG" >&2
+	discoSNPs=$OPTARG
+	;;
+
+	s)
+	echo -e "\t##use distance with the seed : $OPTARG" >&2
+	l=$OPTARG
+	;;
+
+	n)
+	echo -e "\##use number of mismatches : $OPTARG" >&2
+	n=$OPTARG
+	;;
+
+	l)
+	echo -e "\t##use seed length : $OPTARG" >&2
+	s=$OPTARG
+	;;
+
+	f)
+	echo -e "\t##use directly samfile : $OPTARG" >&2
+	samfile=$OPTARG
+	;;
+		
+	o)
+	echo -e "\t##output : $OPTARG" >&2
+	vcffile=$OPTARG
+	;;
+	
+	\?)
+	echo -e "##Invalid option: -$OPTARG" >&2
+	exit 1
+	;;
+
+	:)
+	echo "##Option -$OPTARG requires an argument." >&2
+	exit 1
+	;;
+esac
+done
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------
+###Tests
+if [ -z "$PATH_VCF_creator" ];then
+	PATH_VCF_creator=$DIR"/tools"
+fi
+if [ ! -e  $PATH_VCF_creator/VCF_creator.py ]; then
+	echo "...Unable to find VCF_creator..."
+	exit 1
+fi
+if [ -z "$s" ];then
+	s=0
+fi
+if [ -z "$l" ];then
+	l=10
+fi
+
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------
+####Use the pipeline of aligement
+if [ -z "$samfile" ];then
+	if [ -z "$PATH_BWA" ] ;then
+		IS_BWA=$(command -v bwa)
+	fi
+	if [ -z "$IS_BWA" ];then
+		echo -e "... BWA not found... add bwa to \$PATH or give directly the path (-b)"
+		exit 1
+	else 
+		PATH_BWA=$(dirname $IS_BWA)
+	fi
+	if [ -z "$vcffile" ] || [[ "$vcffile" =~ *.vcf ]]; then
+		echo -e "...You must provide an output <file>.vcf : option -o (for help -h)..."
+		echo -e "...Usage : ./run_pipeline_VCF_creator.sh OPT..."
+		exit 1
+	fi
+	if [ -z "$genome" ]; then
+		echo -e "...You must provide a genome of reference : option -g (for help -h)..."
+		exit 1
+	fi
+	
+	if [ -z "$discoSNPs" ];then
+		echo "... Error : file disco is missing : option -p (for help -h)..."
+		exit 1
+	else
+		if [ ! -e $PATH_VCF_creator/remove_extensions_disco_file.py ];then
+			echo "...Unable to find remove_extensions_disco_file.py..."
+			exit 1
+		else
+			discoSNPsbis=$(basename $discoSNPs .fa)"bis.fasta"
+			python $PATH_VCF_creator/remove_extensions_disco_file.py $discoSNPs $discoSNPsbis
+			if [ -z "$discoSNPsbis" ];then
+				echo "...Error with the script remove_extensions_disco_file.py..."
+			fi
+		fi
+	fi
+	if [ -z "$n" ];then
+		echo -e "\t##Default value for the number of mismatches allowed in alignment : 3 (to change it -n)"
+		n=3
+	fi
+###BWA 
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------
+	#BWA files
+	vcf=$(basename $vcffile .vcf)"_"$(basename $discoSNPs .fasta)"_n"$n"_l"$l"_s"$s".vcf"
+	samfile=$(basename $vcffile .vcf)"_"$(basename $discoSNPs .fa)"_n"$n"_l"$l"_s"$s".sam"
+	saifile=$(basename $vcffile .vcf)"_"$(basename $discoSNPs .fa)"_n"$n"_l"$l"_s"$s".sai"
+	indexamb=$genome".amb"
+	indexann=$genome".ann"
+	indexbwt=$genome".bwt"
+	indexpac=$genome".pac"
+	indexsa=$genome".sa"
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------
+	##Indexation of reference genome
+	if [ -e $indexamb ] && [ -e $indexann ] && [ -e $indexbwt ] && [ -e $indexpac ] && [ -e $indexsa ]; then
+		echo -e "...Indexation : Using the existing index..."
+	else
+		$PATH_BWA/bwa index $genome
+	fi
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------
+	##Alignment disco snps on the reference genome
+	if [ -e $saifile ]; then
+		echo -e "...Alignment : Using the existing file : $saifile"
+	else
+		$PATH_BWA/bwa aln -N -k $s -n $n -l $l $genome $discoSNPsbis > $saifile
+	fi
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------
+	##Creation of the samfile
+	if [ -e $samfile ]; then
+		echo -e "...Samfile already exists. Skipped to vcf creation..."
+	else
+		$PATH_BWA/bwa samse -n 1000 $genome $saifile $discoSNPsbis > $samfile
+	fi 
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------
+	##Creation of the vcf file
+	if [ -e $vcf ]; then
+		echo -e "...VCF file for this alignment already exists...==> $vcf"
+	else 
+		python $PATH_VCF_creator/VCF_creator.py -s $samfile -n $n --o $vcf
+		echo -e "... Creation of the vcf file : done ...==> $vcf"
+	fi	
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------
+	
+else
+####Skip alignment phase to create a vcf file
+##Test to execute VCF_creator
+	echo -e "...Skipping alignment phase..."
+	if [ -z "$vcffile" ] || [[ "$vcffile" =~ *.vcf ]]; then
+		echo -e "...You must provide an output <file>.vcf..."
+		exit 1
+	fi
+	if [ -z "$n" ]; then
+		echo -e "...You must provide the number of mismatch allowed during the alignment phase.."
+		exit 1
+	fi
+	##Creation of the vcf file
+	python $PATH_VCF_creator/VCF_creator.py -s $samfile -n $n --output $vcffile	
+fi
+
+if [ $remove=0 ];then
+	rm -r $saifile $discoSNPsbis
+else
+	rm -r $indexamb $indexann $indexbwt $indexpac $indexsa $saifile $discoSNPsbis
+fi	
+
+
---
+++ tools/VCF_creator.py
@@ -0,0 +1,328 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+###############################################
+#
+#
+#
+import os
+import sys
+import subprocess
+import re
+import time
+import getopt
+from functionsVCF_creator import *
+#Help
+def usage():
+	usage= """
+	################################
+		Run VCF_creator
+	################################
+	
+	-h --help : print this message
+	-s --sam_file : <file>.sam of the alignment
+	-n --mismatch : number of differences allowed in the mapper (BWA)
+	-o --output : vcf file 
+	
+	"""
+	print usage
+
+try:
+	opts, args = getopt.getopt(sys.argv[1:],"h:s:n:o",["help","disco_file","mismatch","output="])
+	if not opts:
+		usage()
+		sys.exit(2)
+except getopt.GetoptError, e:
+	print e
+	usage()
+	sys.exit(2)
+for opt, arg in opts : 
+	if opt in ("-h", "--help"):
+		usage()
+		sys.exit(2)
+	elif opt in ("-s","--sam_file"):
+		fichier = arg
+		if ".sam" in fichier:
+			samfile=open(fichier,'r')
+		else:
+			print "...Unknown file extension for the input. Try with a <file>.sam..."
+			sys.exit(2)
+	elif opt in ("-n","--mismatch"):
+		 nbMismatchBWA= arg
+	elif opt in ("-o","--output"):
+		if ".vcf" in arg:
+			VCF = open(arg,'w')
+		else :
+			print "...Unknown file extension for the output. Try with a <file>.vcf..."
+			sys.exit(2)
+	else:
+		print("Unkwnown option {} ".format(opt))
+		usage()
+		sys.exit(2)
+
+
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------
+###Header of the VCF file 
+today=time.localtime()
+date=str(today.tm_year)+str(today.tm_mon)+str(today.tm_mday)
+VCF.write('##fileformat=VCFv4.1\n')
+VCF.write('##filedate='+str(date)+'\n')
+VCF.write('##REF=<ID=REF,Number=1,Type=String,Description="Allele of the path Disco aligned with the least mismatches ">\n')
+VCF.write('##ALT=<ID=ALT,Number=1,Type=String,Description="Allele of the other path ">\n')
+VCF.write('##FILTER=<ID=MULTIPLE,Number=1,Type=String,Description="Mapping type : PASS or MULTIPLE or \'.\' ">\n')
+VCF.write('##INFO=<ID=Ty,Number=1,Type=Float,Description="SNP, INS, DEL or "."">\n')
+VCF.write('##INFO=<ID=Rk,Number=1,Type=Float,Description="SNP rank">\n')
+VCF.write('##INFO=<ID=DT,Number=1,Type=Integer,Description="Mapping distance with reference">\n')
+VCF.write('##INFO=<ID=UL,Number=1,Type=Integer,Description="Lenght of the unitig left">\n')
+VCF.write('##INFO=<ID=UR,Number=1,Type=Integer,Description="Lenght of the unitig right">\n')
+VCF.write('##INFO=<ID=CL,Number=1,Type=Integer,Description="Lenght of the contig left">\n')
+VCF.write('##INFO=<ID=CR,Number=1,Type=Integer,Description="Lenght of the contig right">\n')
+VCF.write('##INFO=<ID=DP,Number=1,Type=Integer,Description="Combined depth accross samples (average)">\n')
+VCF.write('##INFO=<ID=Genome,Number=1,Type=String,Description="Allele of the reference;for indel reference is <DEL> or <INS>">\n')
+VCF.write('##INFO=<ID=Sd,Number=1,Type=Integer,Description="Reverse (1) or Forward (0) Alignement">\n')
+nbGeno=0
+nbSnp,nbGeno = Comptage(fichier)
+nbCol=9+int(nbGeno)
+table = [[0] * int(nbCol) for _ in range(int(nbSnp))]
+if nbGeno==0:
+	VCF.write('#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\n')
+else:
+	VCF.write('#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t')
+	for i in range(0,int(nbGeno)):
+		nomCol="G"+str(i+1)
+		VCF.write(str(nomCol)+"\t")
+		if i==int(nbGeno)-1:
+			VCF.write(str(nomCol)+"\n")			
+snpAvant=None
+line=0
+mul=0
+nbOk=0
+nbmulti=0
+nbrupture1=0
+nbrupture2=0
+nbrupture3=0
+nbrupturedmax=0
+nbrupture1=0
+nbrupture0=0
+pb=0
+nbunmapped=0
+if ".sam" in fichier:
+	for snp in samfile :
+		if '@' != snp[0] :
+			if snpAvant is not None:
+				table[line][8]="."
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------
+				#Check if the snps are from the same path
+				numSNP1=ParsingDiscoSNP(snp,1)
+				numSNP2=ParsingDiscoSNP(snpAvant,1)			
+				getcouple=isCouple(numSNP1,numSNP2)
+				if getcouple==1:
+					dicoHeaderUp={}
+					dicoHeaderLow={}
+					#Get DiscoSnp Variables
+					if "higher" in snp:
+						snpUp,numSNPUp,unitigLeftUp,unitigRightUp,contigLeftUp,contigRightUp,valRankUp,listeCouvertureUp,listeCUp,nb_polUp,lnUp,posDUp,ntUp,ntLow,genoUp,dicoHeaderUp=ParsingDiscoSNP(snp,0)
+					else:
+						snpLow,numSNPLow,unitigLeftLow,unitigRightLow,contigLeftLow,contigRightLow,valRankLow,listeCouvertureLow,listeCLow,nb_polLow,lnLow,posDLow,ntUp,ntLow,genoLow,dicoHeaderLow=ParsingDiscoSNP(snp,0)
+								
+					if "higher" in snpAvant:					
+						snpUp,numSNPUp,unitigLeftUp,unitigRightUp,contigLeftUp,contigRightUp,valRankUp,listeCouvertureUp,listeCUp,nb_polUp,lnUp,posDUp,ntUp,ntLow,genoUp,dicoHeaderUp=ParsingDiscoSNP(snpAvant,0)
+					else:
+						snpLow,numSNPLow,unitigLeftLow,unitigRightLow,contigLeftLow,contigRightLow,valRankLow,listeCouvertureLow,listeCLow,nb_polLow,lnLow,posDLow,ntUp,ntLow,genoLow,dicoHeaderLow=ParsingDiscoSNP(snpAvant,0)
+					print "************"
+					print snpUp
+					print snpLow
+					
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------				
+					#Information on coverage by dataset
+					couvUp,couvLow=GetCoverage(listeCUp,listeCLow,listeCouvertureUp,listeCouvertureLow)
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------				
+					#Variables
+					comptPol=0
+					info=None
+					multi=None
+					ok=None
+					dmax=0
+					indel=0
+					champFilter='.'
+					posUp,posLow,snpLow,snpUp,boolMapUp,boolMapLow,boolXAUp,boolXALow = GetCouple(snpUp,snpLow)
+					seqUp=snpUp[9]
+					seqLow=snpLow[9]
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------					
+					#Validation of a couple of SNPs 
+					NM=0
+					for NM in range(0,int(nbMismatchBWA)+1):
+						couple=ValidationSNP(snpLow,posLow,snpUp,posUp,NM)
+						if couple== "ok" or couple == "multiple":
+							rupture=NM
+							break
+					if rupture==nbMismatchBWA:
+						if snpLow[1]=="4":
+							dmax=1
+						elif snpUp[1]=="4":
+							dmax=1
+						else:
+							dmax==0
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------
+					#VCF champ INFO Multi
+					if boolXAUp==1 and boolXALow==1:
+						multi="multi"
+						mul+=1
+					elif boolXAUp==0 and boolXALow==0:
+						multi="none"
+					elif (boolXAUp==1 and boolXALow==0) or  (boolXAUp==0 and boolXALow==1):
+						multi="one"
+						mul+=1
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------
+					#VCF champs Filter
+					if couple=="ok":
+						champFilter="PASS"			
+						ok=str(NM)
+					
+					elif couple == "unmapped":
+						champFilter="."
+						ok=-1
+					elif couple == "multiple":
+						champFilter="MULTIPLE"
+						ok=-1
+					else : 
+						champFilter="probleme...."
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------
+					#Ecriture dans le vcf
+					if "SNP" in snpUp[0] and "SNP" in snpLow[0]:
+#---------------------------------------------------------------------------------------------------------------------------
+						indel=0
+						#Vérification du nombre de polymorphisme
+						listePolymorphismePos=[]
+						#Récupération des positions de mapping associées à leur nombre de mutation
+						if (int(nb_polLow)>=2) or (int(nb_polUp)>=2):
+							listePolymorphismePos,listeLettreUp,listeLettreLow,listePosR,listeLettreUpR,listeLettreLowR=GetPolymorphisme(dicoHeaderUp,seqUp,indel)
+						#Remplissage du vcf selon 2 cas :
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------
+						##SNP simple
+						if len(listePolymorphismePos)==0:
+							tp="SNP"
+							table[line][6]=champFilter
+							posModif=len(snpUp[9])/2
+							lettreLow,positionSnpLow,lettreUp,positionSnpUp,boolRefLow,boolRefUp,bug,erreur,reverseUp,reverseLow,lettreRefUp,lettreRefLow = RecupPosSNP(snpUp,snpLow,posUp,posLow,nb_polUp,nb_polLow,dicoHeaderUp,indel)
+							#Création VCF
+							table,boolRefUp,boolRefLow=RemplissageLigneVCF(snpUp,snpLow,lettreLow,positionSnpLow,lettreUp,positionSnpUp,boolRefLow,boolRefUp,table,nbSnp,line,bug,erreur,dmax)
+							table=GetGenotype(genoUp,boolRefUp,boolRefLow,table,line,nbGeno)
+							#Remplissage du champ info en fonction de la référence
+							if boolRefUp==1:
+								info="Ty:"+str(tp)+";"+"Rk:"+str(valRankUp)+";"+"MULTI:"+str(multi)+";"+"DT:"+str(ok)+";"+"UL:"+str(unitigLeftUp)+";"+"UR:"+str(unitigRightUp)+";"+"CL:"+str(contigLeftUp)+";"+"CR:"+str(contigRightUp)+";"+str(couvUp)+";"+"Genome:"+str(lettreRefUp)+";"+"Sd:"+str(reverseUp)
+							else:
+								info="Ty:"+str(tp)+";"+"Rk:"+str(valRankLow)+";"+"MULTI:"+str(multi)+";"+"DT:"+str(ok)+";"+"UL:"+str(unitigLeftLow)+";"+"UR:"+str(unitigRightLow)+";"+"CL:"+str(contigLeftLow)+";"+"CR:"+str(contigRightLow)+";"+str(couvLow)+";"+"Genome:"+str(lettreRefLow)+";"+"Sd:"+str(reverseLow)
+							if dmax==1:
+								table[line][7]=info+";"+"DMax:"+str(dmax)
+							else:
+								table[line][7]=info	
+				
+						else: 
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------
+							##SNPs proches
+							indel=0
+							dicoUp={}
+							dicoLow={}
+							posModif=None
+							dicoUp,dicoLow,listePolymorphismePosUp,listePolymorphismePosLow=RecupPosSNP(snpUp,snpLow,posUp,posLow,nb_polUp,nb_polLow,dicoHeaderUp,indel)
+							table,line,boolRefUp,boolRefLow=RemplissageVCFSNPproches(dicoUp,dicoLow,table,champFilter,dmax,snpUp,snpLow,listePolymorphismePosUp,listePolymorphismePosLow,listePolymorphismePos,line,multi,ok,couvUp,couvLow,listeLettreUp,listeLettreLow,genoUp,nbGeno)
+							snpAvant=snp
+							continue
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------
+					#Cas des Indels					
+					if "INDEL" in snpUp[0] and "INDEL" in snpLow[0]:
+						indel=1
+						table[line][6]=champFilter
+						seqInsert=0
+						seqUp=snpUp[9]
+						seqLow=snpLow[9]
+						if len(seqUp)<len(seqLow):
+							seq=seqLow
+						else:
+							seq=seqUp
+						listePos,listePosR,insert,ntStart,ambiguity=GetPolymorphisme(dicoHeaderUp,seq,indel)
+						snpUp,numSNPUp,unitigLeftUP,unitigRightUp,contigLeftUp,contigRightUp,valRankUp,listeCouvertureUp,listeCUp,nb_polUp,lnUp,posDUp,ntUp,ntLow,genoUp,dicoHeaderUp=ParsingDiscoSNP(snpUp,0)
+						snpLow,numSNPLow,unitigLeftLow,unitigRightLow,contigLeftLow,contigRightLow,valRankLow,listeCouvertureLow,listeCLow,nb_polLow,lnLow,posDLow,ntUp,ntLow,genoLow,dicoHeaderLow=ParsingDiscoSNP(snpLow,0)
+						lettreLow,positionSnpLow,lettreUp,positionSnpUp,boolRefLow,boolRefUp,bug,erreur,reverseUp,reverseLow,lettreRefUp,lettreRefLow= RecupPosSNP(snpUp,snpLow,posUp,posLow,nb_polUp,nb_polLow,dicoHeaderUp,indel)
+						if len(seqUp)<len(seqLow) and reverseUp==0:
+							lettreLow=insert
+							lettreUp=ntStart
+						else:
+							lettreUp=insert
+							lettreLow=ntStart
+						if boolRefUp==1:
+							if len(lettreUp)==len(insert):
+								lettreRefUp="."
+								tp="INS"
+							else:
+								lettreRefUp="."
+								tp="DEL"
+							table[line][0]=snpUp[2]					
+							table[line][1]=int(positionSnpUp)-1
+							table[line][2]=numSNPUp
+							table[line][3]=lettreUp
+							table[line][4]=lettreLow
+							table=GetGenotype(genoUp,1,0,table,line,nbGeno)
+							if snpUp[10]=="*":
+								table[line][5]="."
+							else:
+								table[line][5]=snpUp[10]
+							info="Ty:"+str(tp)+";"+"Rk:"+str(valRankUp)+";"+"MULTI:"+str(multi)+";"+"DT:"+str(ok)+";"+"UL:"+str(unitigLeftUp)+";"+"UR:"+str(unitigRightUp)+";"+"CL:"+str(contigLeftUp)+";"+"CR:"+str(contigRightUp)+";"+str(couvUp)+";"+"Genome:"+str(lettreRefUp)+";"+"Sd:"+str(reverseUp)
+						elif boolRefLow==1:
+							if len(lettreLow)==len(insert):
+								lettreRefLow="."
+								tp="INS"
+							else:
+								lettreRefLow="."
+								tp="DEL"
+							table[line][0]=snpLow[2]	
+							table[line][1]=int(positionSnpLow)-1
+							table[line][2]=numSNPLow
+							table[line][3]=lettreLow
+							table[line][4]=lettreUp
+							table=GetGenotype(genoUp,0,1,table,line,nbGeno)
+							if snpLow[10]=="*":
+								table[line][5]="."
+							else:
+								table[line][5]=snpLow[10]
+							info="Ty:"+str(tp)+";"+"Rk:"+str(valRankLow)+";"+"MULTI:"+str(multi)+";"+"DT:"+str(ok)+";"+"UL:"+str(unitigLeftLow)+";"+"UR:"+str(unitigRightLow)+";"+"CL:"+str(contigLeftLow)+";"+"CR:"+str(contigRightLow)+";"+str(couvLow)+";"+"Genome:"+str(lettreRefLow)+";"+"Sd:"+str(reverseLow)
+						else:
+							if lettreUp==refLettre or lettreUp==refLettreR:
+								lettreRefUp="<DEL>"
+							else:
+								lettreRefUp="<INS>"
+							table[line][0]=snpUp[2]					
+							table[line][1]=pos
+							table[line][2]=numSNPUp
+							table[line][3]=lettreUp
+							table[line][4]=lettreLow
+							table=GetGenotype(genoUp,1,0,table,line,nbGeno)
+							table[line][5]=snpUp[10]
+						table[line][7]=info
+					print table[line]
+					line+=1
+			snpAvant=snp	
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------
+#Ecriture fichier
+for line in table :
+	for element in line:			
+		VCF.write(str(element))
+		VCF.write("\t")
+	VCF.write('\n')
+VCF.close()
+samfile.close()
+
