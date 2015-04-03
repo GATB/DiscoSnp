83cf0d41a75d4db8ea5cd593cafbf3aec0b4cb60
chloe riou <chloe.riou@inria.fr>
2015-04-03 09:34:02 +0200

    Modifications of tools/one2zeroBased_vcf.py

--- tools/one2zeroBased_vcf.py
+++ tools/one2zeroBased_vcf.py
@@ -4,13 +4,15 @@
 #Transforms a 1-based vcf into 0-based vcf
 # usage : one2zeroBased_vcf.py vcf_file
 import sys
-
+from functionsVCF_creator import *
 file=open(sys.argv[1],"r")
+VCFfile=open("VCFone2zeroBAsed.vcf","w")
 
 for line in file:
     if "#" == line[0]: 
         print line, 
         continue
+    line=line.rstrip('\n')
     #SNP_higher_path_3       117     3       C       G       .       .       Ty=SNP;Rk=1.00000;UL=86;UR=261;CL=.;CR=.;C1=124,0;C2=0,134      GT:DP:PL        0/0:124:10,378,2484     1/1:134:2684,408,10
     splitted_line = line.split("\t")
     
@@ -19,7 +21,8 @@ for line in file:
         splitted_line[1]=int(splitted_line[1])-1
 
     # prints the line
-    for i in range(len(splitted_line)):
-        print splitted_line[i],
-        if i<len(splitted_line)-1:
-            print "\t",
+    printOneline(splitted_line,VCFfile)
+    #for i in range(len(splitted_line)):
+    #    print splitted_line[i],
+    #    if i<len(splitted_line)-1:
+    #        print "\t",
