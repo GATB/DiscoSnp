5d324f512ec545516723a8bbe9888133095bc83d
chloe riou <chloe.riou@inria.fr>
2015-04-28 10:56:25 +0200

    correction in ReferenceChecker : index of list

--- run_VCF_creator.sh
+++ run_VCF_creator.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-
+#*****************************************************************************
+#   VCF_Creator: mapping and VCF creation feature in DiscoSnp++
+#   Copyright (C) 2015  INRIA
+#   Author: C.Riou
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#*****************************************************************************
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 remove=0
