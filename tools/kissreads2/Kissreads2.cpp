0712f9c2451b4eddab5d0d33ec82f52059c87965
Pierre Peterlongo <ppeterlo@cl1n026.genouest.org>
2015-07-13 21:07:45 +0200

    cleaning files

--- tools/kissreads2/Kissreads2.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Kissreads2.cpp
-//  discoSnp_GATB
-//
-//  Created by Pierre Peterlongo on 03/07/15.
-//  Copyright (c) 2015 Pierre Peterlongo. All rights reserved.
-//
-
-#include "Kissreads2.h"
