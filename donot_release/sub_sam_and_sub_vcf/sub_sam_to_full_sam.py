14b47813d578d8bea294ac003ad57ecac75795ee
Pierre Peterlongo <pierre.peterlongo@inria.fr>
2015-09-23 22:18:37 +0200

    VCF creator now outputs the filtered SAM, and python scripts may deal with sub sampled reference genomes for finding back real genomic locations

---
+++ donot_release/sub_sam_and_sub_vcf/sub_sam_to_full_sam.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+
+import sys
+import getopt
+
+
+# Given a SAM formated as 
+# INDEL_lower_path_390|P_1:30_3_9|high|nb_pol_1|C1_2148|C2_1714|Q1_69|Q2_68|G1_1/1:41976,5624,67|G2_1/1:33373,4389,86|rank_0.01396        16      chr11:530242..537550    4033    37      54M     *       0       0       TCTTGCCCACACCGCCGGCGCCCACCACCACCAGCTTATATTCCGTCATCGCTC  *       XT:A:U  NM:i:0  X0:i:1  X1:i:0  XM:i:0  XO:i:0  XG:i:0  MD:Z:54
+# Transform it as:
+# INDEL_lower_path_390|P_1:30_3_9|high|nb_pol_1|C1_2148|C2_1714|Q1_69|Q2_68|G1_1/1:41976,5624,67|G2_1/1:33373,4389,86|rank_0.01396        16      chr11    534285    37      54M     *       0       0       TCTTGCCCACACCGCCGGCGCCCACCACCACCAGCTTATATTCCGTCATCGCTC  *       XT:A:U  NM:i:0  X0:i:1  X1:i:0  XM:i:0  XO:i:0  XG:i:0  MD:Z:54
+# that is to say: put back the right position: 4043+530242 (beginning of the portion of mapped sequence, starting position 530242 on the chromosome 11
+
+
+#A difficulity stands in the fact that for each line, maybe it does not contain any position information. In this case we don't modify the line
+
+
+filein=open(sys.argv[1],'r')
+
+for line in filein: 
+    if line[0]=='@':
+        print line,
+        continue
+    values=line.split()
+
+    if not ':' in values[2]: # no position information, we continue
+        print line, 
+        continue
+        
+    start=int(values[2].split(':')[1].split('.')[0])
+    position=int(values[3])+start
+    for i in xrange(2):
+        print values[i]+"\t",
+    print values[2].split(':')[0]+"\t"+str(position),
+    for i in xrange(4,len(values)):
+        print values[i]+"\t",
+    print
--- tools/VCF_creator.py
+++ tools/VCF_creator.py
@@ -34,6 +34,8 @@ nbMismatchBWA=3 #number of mismatch allowed for the alignment in BWA
 listName=[] #List from the file name used to create the VCF
 nbSnp=0 #number of SNP in the input file
 nbGeno=0 #number of genotype for every path
+filtered_sam=False
+filtered_sam_file=""
 #Help
 def usage():
     usage= """
@@ -45,12 +47,13 @@ def usage():
     -s --sam_file : <file>.sam of the alignment
     -n --mismatch : number of differences allowed in the mapper (BWA)
     -o --output : vcf file 
+    -f --output_filtered_SAM : if provided, a SAM file in which uncorrectly mapped prediction (corresponding to filter '.' in the provided VCF) are removed is output in this file.
     
     """
     print usage
 ###OPTIONS 
 try:
-    opts, args = getopt.getopt(sys.argv[1:],"h:s:n:o:",["help","disco_file","mismatch","output="])
+    opts, args = getopt.getopt(sys.argv[1:],"h:s:n:o:f:",["help","disco_file","mismatch","output=","output_filtered_SAM"])
     if not opts:
         usage()
         sys.exit(2)
@@ -84,6 +87,13 @@ for opt, arg in opts :
         else :
                 print "!! No output !!"
                 sys.exit(2)      
+    elif opt in ("-f","--output_filtered_SAM"):
+        if arg!=None:
+            filtered_sam = True
+            filtered_sam_file = open(arg,'w')
+        else:
+            print "!! No filtered sam output !!"
+            sys.exit(2)      
     else:
         print("Unkwnown option {} ".format(opt))
         usage()
@@ -100,7 +110,10 @@ if ".sam" in fileName: #Checks if it's a samfile
         while True:
                 line1=samfile.readline()
                 if not line1: break #End of file
-                if line1.startswith('@'): continue #We do not read headers
+                if line1.startswith('@'): 
+                    if filtered_sam:
+                        filtered_sam_file.write(line1)
+                    continue #We do not read headers
                 line2=samfile.readline() #Read couple of lines
                 #Initializes variant object with the samline
                 variant_object, vcf_field_object=InitVariant(line1,line2) #Fills the object with the line of the samfile        
@@ -108,6 +121,11 @@ if ".sam" in fileName: #Checks if it's a samfile
                         sys.exit(1)
                 #Checks the mapping on reference and determines the shift with the reference, which path is the reference ...
                 table=MappingTreatement(variant_object,vcf_field_object,nbGeno)
+                #Added by Pierre Peterlongo, Sept 2015. Outputs a new SAM file corresponding to mapped sequences (PASS or MULTIPLE)
+                if(filtered_sam and vcf_field_object.filterField!="."):
+                    filtered_sam_file.write(line1)
+                    filtered_sam_file.write(line2)
+                
                 #Fills the VCF file
                 variant_object.FillVCF(VCFFile,nbGeno,table,vcf_field_object)
                   
@@ -130,7 +148,9 @@ elif ".fa" in fileName: #Treatement of the fasta file (no mapping information)
 
 
 VCFFile.close()
-samfile.close()    
+samfile.close()
+if filtered_sam:
+    filtered_sam_file.close()
 
 
 
