f80969f84ccc378d3cbc41de3eb850ca5d2c9184
PETERLONGO Pierre <pierre.peterlongo@inria.fr>
2018-09-25 16:17:35 +0200

    adding script split_multiple_snps.py

--- scripts/split_multiple_snps.py
+++ scripts/split_multiple_snps.py
@@ -55,18 +55,19 @@ while True:
     str_nb_pol=com1.split('|')[3]
     assert str_nb_pol.startswith("nb_pol")
     nb_pol=int(com1.split('|')[3].split('_')[-1])
-    # if nb_pol==1:
-    #     print (com1)
-    #     print (seq1)
-    #     print (com2)
-    #     print (seq2)
-    #     continue
+    if com1.startswith(">INDEL"):
+        print (com1)
+        print (seq1)
+        print (com2)
+        print (seq2)
+        continue
     
     all_pos_pol=com1.split('|')[1].split(',')
     splited_com1=com1.split('|')
     splited_com2=com2.split('|')
     for pol in range(nb_pol): 
         pos_pol=int(all_pos_pol[pol].split(':')[1].split('_')[0])
+        print (com1)
         alt_allele=all_pos_pol[pol].split(':')[1].split('_')[1].split('/')[1]
         subseq1=seq1[pos_pol-delta:pos_pol+delta+1]                                     # get the upper case sequence, including the current variant and containing the sequence surrounding this variant
         subseq2=seq1[pos_pol-delta:pos_pol]+alt_allele+seq1[pos_pol+1:pos_pol+delta+1]  # get the upper case sequence, including the current variant and containing the sequence from sequence1 containing this variant. 
