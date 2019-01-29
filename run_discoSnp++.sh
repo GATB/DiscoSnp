1d0667baf7d123f9aed27bb162a384c68c41a377
PETERLONGO Pierre <pierre.peterlongo@inria.fr>
2019-01-29 15:43:25 +0100

    ghost push Closes #4 Closes #10 Closes #7 Closees #9

--- run_discoSnp++.sh
+++ run_discoSnp++.sh
@@ -18,7 +18,7 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #*****************************************************************************
-
+ 
 function myrealpath { echo $(cd $(dirname $1); pwd)/$(basename $1); }
 
 option_cores_gatb=""
