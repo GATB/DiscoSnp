0621bff93c701066a4f310999768963b31d1857c
Pierre Peterlongo <pierre.peterlongo@inria.fr>
2020-01-20 11:52:11 +0100

    adding missing fields for dat format representation of the variant graph

--- scripts/k3000/K3000_gfa_to_dat.py
+++ scripts/k3000/K3000_gfa_to_dat.py
@@ -4,110 +4,84 @@ import K3000_gfa_post_treatment as gpt # :)
 
 
 """
-Dat file: 
-AcorusCalamus_multigraph.dat
-
 data;
-param Title := AcorusCalamus;
-param start := "3_0_F";
+#id of forward and reverse nodes
 set V :=
-3_0_F
-1_0_R
-0_0_R
-0_0_F
-1_0_F
-1_1_F
-3_0_R
-1_1_R
+p0
+p1
+p2
+m0
+m1
+m2
 ;
+param number_loci
+p0	3
+p1	2
+p2	2
+m0 	3
+m1	2
+m2	2
+;
+#id of nodes with their coverage. Here (3 sept 2019) coverage refers to the read coverage of the less covered allele of all alleles of the fact
 param w :=
-3_0_F	83691
-1_0_R	26025
-0_0_R	18476
-0_0_F	18476
-1_0_F	26025
-1_1_F	26025
-3_0_R	83691
-1_1_R	26025
+p0	5626
+m0  5626
+p1	10152
+m1	10152
+p2	1142
+m2	1142
 ;
+#for each forward node, indicates the id of the reverse version
 set reverse :=
-3_0_F	3_0_R
-1_0_R	1_0_F
-0_0_R	0_0_F
-0_0_F	0_0_R
-1_0_F	1_0_R
-1_1_F	1_1_R
-3_0_R	3_0_F
-1_1_R	1_1_F
+p0	m0
+p1	m1
+p2	m2
 ;
+#set of edges. Four types of edges, 1/ "overlaps" edges, that show an overlap between facts and 2/ "links" edges, that represent facts linked by paired reads (distanace unknown) and 3/ "successive" edges that represent two successive facts (without phasing) and 4/ "incompatible" edges, no path should contain two nodes linked by such an edge 
 set Edges :=
-3_0_F	1_1_F	overlaps
-3_0_F	1_1_F	links
-3_0_F	1_0_F	overlaps
-3_0_F	1_0_F	links
-1_0_R	3_0_F	overlaps
-1_0_R	3_0_F	links
-1_0_R	3_0_R	overlaps
-1_0_R	3_0_R	links
-0_0_R	1_0_R	overlaps
-0_0_R	1_0_R	links
-0_0_R	1_1_R	overlaps
-0_0_R	1_1_R	links
-0_0_F	1_0_R	overlaps
-0_0_F	1_0_R	links
-0_0_F	1_1_R	overlaps
-0_0_F	1_1_R	links
-1_0_F	0_0_R	overlaps
-1_0_F	0_0_R	links
-1_0_F	0_0_F	overlaps
-1_0_F	0_0_F	links
-1_1_F	0_0_R	overlaps
-1_1_F	0_0_R	links
-1_1_F	0_0_F	overlaps
-1_1_F	0_0_F	links
-3_0_R	1_1_F	overlaps
-3_0_R	1_1_F	links
-3_0_R	1_0_F	overlaps
-3_0_R	1_0_F	links
-1_1_R	3_0_F	overlaps
-1_1_R	3_0_F	links
-1_1_R	3_0_R	overlaps
-1_1_R	3_0_R	links
+p0	m55	overlaps #TODO (plus tard): il faut aussi doubler les arêtes et donc générer p55	m0	overlaps (pas d'ordre à respecter au sein de chaque champ)
+p0	m53	overlaps
+p1	m55	overlaps
+p303	p305	links
+p303	p310	links
+p303	p304	links
+p0	p54	incompatibles
+p0	p1	incompatibles
+p0	p2	incompatibles
+p243	p341	successive
+p244	p341	successive
+p320	m388	successive
 ;
+#overlap length of each edge. For an "overlaps" edge, it indicates the number of common variants. For any other edge type (links, successive, or incompatibles), this is set to zero
 param l :=
-3_0_F	1_1_F	overlaps	-99
-3_0_F	1_1_F	links	0
-3_0_F	1_0_F	overlaps	-99
-3_0_F	1_0_F	links	0
-1_0_R	3_0_F	overlaps	-99
-1_0_R	3_0_F	links	0
-1_0_R	3_0_R	overlaps	-99
-1_0_R	3_0_R	links	0
-0_0_R	1_0_R	overlaps	-99
-0_0_R	1_0_R	links	0
-0_0_R	1_1_R	overlaps	-99
-0_0_R	1_1_R	links	0
-0_0_F	1_0_R	overlaps	-99
-0_0_F	1_0_R	links	0
-0_0_F	1_1_R	overlaps	-99
-0_0_F	1_1_R	links	0
-1_0_F	0_0_R	overlaps	-99
-1_0_F	0_0_R	links	0
-1_0_F	0_0_F	overlaps	-99
-1_0_F	0_0_F	links	0
-1_1_F	0_0_R	overlaps	-99
-1_1_F	0_0_R	links	0
-1_1_F	0_0_F	overlaps	-99
-1_1_F	0_0_F	links	0
-3_0_R	1_1_F	overlaps	-99
-3_0_R	1_1_F	links	0
-3_0_R	1_0_F	overlaps	-99
-3_0_R	1_0_F	links	0
-1_1_R	3_0_F	overlaps	-99
-1_1_R	3_0_F	links	0
-1_1_R	3_0_R	overlaps	-99
-1_1_R	3_0_R	links	0
+p0	m55	overlaps	3
+p0	m53	overlaps	3
+p1	m55	overlaps	3
+p303	p305	links	0
+p303	p310	links	0
+p303	p304	links	0
+p0	p54	incompatibles	0
+p0	p1	incompatibles	0
+p0	p2	incompatibles	0
+p243	p341	successive	0
+p244	p341	successive	0
+p320	m388	successive	0
 ;
+#Coverage of the pairend links. Note that overlap links do not have any coverage (just computed from fact overlaps). Also incompatible and successive links do not have coverage, by definition. 
+param pairend_end_links_coverage :=
+p0	m55	overlaps	0
+p0	m53	overlaps	0
+p1	m55	overlaps	0
+p303	p305	links	12
+p303	p310	links	3
+p303	p304	links	7
+p0	p54	incompatibles	0
+p0	p1	incompatibles	0
+p0	p2	incompatibles	0
+p243	p341	successive	0
+p244	p341	successive	0
+p320	m388	successive	0
+
 """
 
 def print_header():
@@ -117,7 +91,7 @@ def print_header():
 
 
 def print_nodes(gfa_file_name, DG=None):
-    print("#id of forward nodes")
+    print("#id of plus (p) and minus (m) nodes")
     print("set V :=")
     gfa_file = open(gfa_file_name)
     for line in gfa_file.readlines():
@@ -128,11 +102,27 @@ def print_nodes(gfa_file_name, DG=None):
             node_id = line.split()[1]
             if DG and node_id not in DG.nodes(): continue       # this node was removed during gfa post treatment
             print("p"+node_id)  # 'p' stands for "plus strand"
+            print("m"+node_id)  # 'p' stands for "minus strand"
     print(";")
     gfa_file.close()
     
+def print_nodes_number_loci(gfa_file_name, DG=None):
+    print("#id of nodes with their number of variants")
+    print("param w :=")
+    gfa_file = open(gfa_file_name)
+    for line in gfa_file.readlines():
+        line=line.strip()
+        if line[0]=="S":
+            node_id=line.split()[1]
+            if DG and node_id not in DG.nodes(): continue       # this node was removed during gfa post treatment
+            "S       0       28175h;10031h;12786h;-41223l;-26670h; SP:0_426;383_541;427_586;542_661;587_731; BP:0_93;-17_61;54_61;-16_61;14_84;      FC:i:64 RC:i:21"
+            print("p"+node_id+"\t"+str(len(line.split()[2].strip(";").split(";"))))
+            print("m"+node_id+"\t"+str(len(line.split()[2].strip(";").split(";"))))
+    print(";")
+    gfa_file.close()
+
 def print_nodes_weight(gfa_file_name, DG=None):
-    print("#id of forward nodes with their coverage. Here (3 sept 2019) coverage refers to the read coverage of the less covered allele of all alleles of the fact")
+    print("#id of nodes with their coverage. Here (3 sept 2019) coverage refers to the read coverage of the less covered allele of all alleles of the fact")
     print("param w :=")
     gfa_file = open(gfa_file_name)
     for line in gfa_file.readlines():
@@ -157,6 +147,7 @@ def print_reverse(gfa_file_name,DG=None):
             node_id=line.split()[1]
             if DG and node_id not in DG.nodes(): continue       # this node was removed during gfa post treatment
             print("p"+node_id+"\t"+"m"+line.split()[1])  # 'p' stands for "plus strand"
+            print("m"+node_id+"\t"+"m"+line.split()[1])  # 'm' stands for "minus strand"
     print(";")
     gfa_file.close()
     
@@ -206,6 +197,37 @@ def print_edges(gfa_file_name, DG=None):
     print(";")
     gfa_file.close()
     
+def print_edge_coverages(gfa_file_name, DG=None):
+    print ("#Coverage of the pairend links. Note that overlap links do not have any coverage (just computed from fact overlaps). Also incompatible and successive links do not have coverage, by definition. ")
+    print ("param pairend_end_links_coverage :=")
+    gfa_file = open(gfa_file_name)
+    for line in gfa_file.readlines():
+        line=line.strip()
+        if line[0]=="L":
+            "L      1       -       29384   +       8M"
+            "to"
+            "m1	p29384	overlaps 8"
+
+            source_id = line.split()[1]
+            target_id = line.split()[3]
+            if DG:
+                if source_id not in DG.nodes() or target_id not in DG.nodes(): continue # one of these nodes was removed during gfa post treatment
+            overlap_len = int(line.split()[5].rstrip("M"))
+            sign_source="p"
+            if line.split()[2]=='-': sign_source="m"
+            sign_target="p"
+            if line.split()[4]=='-': sign_target="m"
+            type="overlaps"
+            coverage=0
+            if overlap_len==0: 
+                type="links"
+                coverage = int(line.split()[6].split(":")[-1])
+            if overlap_len==-1: type="successive"
+            if overlap_len==-2: type="incompatibles"
+            print(sign_source+source_id+"\t"+sign_target+target_id+"\t"+type+"\t"+str(coverage))  
+            # do not print other edges (unitig linked and snp linked)
+    print(";")
+    gfa_file.close()
     
 def print_edges_content(gfa_file_name, DG=None):
     print("#overlap length of each edge. For an \"overlaps\" edge, it indicates the number of common variants. For any other edge type (links, successive, or incompatibles), this is set to zero")
@@ -258,11 +280,13 @@ def main(gfa_file_name):
     
     print_header()
     print_nodes(gfa_file_name,DG)
+    print_nodes_number_loci(gfa_file_name, DG=None)
     print_nodes_weight(gfa_file_name,DG)
     print_reverse(gfa_file_name,DG)
     if DG:
         print_nodes_connected_components(gfa_file_name, DG)
     print_edges(gfa_file_name,DG)
+    print_edge_coverages(gfa_file_name, DG)
     print_edges_content(gfa_file_name,DG)
 
 
